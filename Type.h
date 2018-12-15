3184acd936dd257ce8bf9628ec93bd1c7fd08b32
dia_maisun <dia_maisun@jcom.home.ne.jp>
2018-12-15 11:16:56 +0900

    .

--- Type.h
+++ Type.h
@@ -7,24 +7,24 @@ using i8 = signed char;
 using i16 = signed short;
 using i32 = signed int;
 using i64 = signed long long;
-#define	I8	"%hhd"
-#define	I16	"%hd"
-#define	I32	"%d"
-#define	I64	"%lld"
+#define I8  "%hhd"
+#define I16 "%hd"
+#define I32 "%d"
+#define I64 "%lld"
 
 using u8 = unsigned char;
 using u16 = unsigned short;
 using u32 = unsigned int;
 using u64 = unsigned long long;
-#define	U8	"%hhu"
-#define	U16	"%hu"
-#define	U32	"%u"
-#define	U64	"%llu"
+#define U8  "%hhu"
+#define U16 "%hu"
+#define U32 "%u"
+#define U64 "%llu"
 
 using f32 = float;
 using f64 = double;
-#define	F32	"%f"
-#define	F64	"%f"
+#define F32 "%f"
+#define F64 "%f"
 #endif//]
 
 
@@ -34,22 +34,22 @@ using i8 = signed char;
 using i16 = signed short;
 using i32 = signed int;
 using i64 = signed long int;
-#define	I8	"%hhd"
-#define	I16	"%hd"
-#define	I32	"%d"
-#define	I64	"%ld"
+#define I8  "%hhd"
+#define I16 "%hd"
+#define I32 "%d"
+#define I64 "%ld"
 
 using u8 = unsigned char;
 using u16 = unsigned short;
 using u32 = unsigned int;
 using u64 = unsigned long int;
-#define	U8	"%hhu"
-#define	U16	"%hu"
-#define	U32	"%u"
-#define	U64	"%lu"
+#define U8  "%hhu"
+#define U16 "%hu"
+#define U32 "%u"
+#define U64 "%lu"
 
 using f32 = float;
 using f64 = double;
-#define	F32	"%f"
-#define	F64	"%f"
+#define F32 "%f"
+#define F64 "%f"
 #endif//]
